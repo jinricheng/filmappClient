cc3f30768ad756d4bce2b6ed9826d2e80a481714
rgarcia <rgarcia@cambridgesemantics.com>
2014-11-26 00:01:19 -0500

    Added tabbed view of the greetings, which now include also author (e-mail) and date. New greetings can be added using a form with AngularJS validation and a greeting preview. TODO: add connection to server side, currently working with greetings mocked at the client using JSON var

--- app/js/app.js
+++ app/js/app.js
@@ -3,12 +3,29 @@
  */
 
 (function(){
-    var greetings = [{"id":1,"content":"Hello World!"},{"id":2,"content":"Bye bye!"}];
+    var greetings = [
+        {"id":1, "content":"Hello World!", "user":"determined@greetings.com", "when":Date.UTC(1970,1,1)},
+        {"id":2, "content":"This is a way to extend the farewell before saying... goodbye!",
+            "user":"hesitant@greetings.com", "when":Date.UTC(2000,1,1)} ];
 
     var app = angular.module("greetingsJS", [ ]);
 
     app.controller('GreetingsController', function(){
         this.greetings = greetings;
+
+
+        this.clearGreeting = function(){
+            return {"content":"Greeting text...", "user":"Your E-Mail", "when":Date.now()};
+        }
+
+        this.newGreeting = this.clearGreeting();
+
+        this.addGreeting = function(){
+            this.newGreeting.id = greetings.length + 1;
+            this.newGreeting.when = Date.now();
+            this.greetings.push(this.newGreeting);
+            this.newGreeting = this.clearGreeting();
+        };
     });
 
     app.controller('TabController', function(){
