9b6a8efddce7f2de6e813bd34db7b5dd063ea283
rgarcia <rgarcia@cambridgesemantics.com>
2014-11-27 09:30:02 -0500

    Refactored index.html code by introducing the greetingTab directive and the corresponding greeting-tab.html partial. Restructured folders following the https://github.com/angular/angular-seed proposed structure: app.js, app.css and index.html in the "app" folder. Then, one subfolder per view/feature. TODO: move greetingTab code from angular.js to the greeting-tab folder

--- app/app.js
+++ app/app.js
@@ -28,15 +28,22 @@
         };
     });
 
-    app.controller('TabController', function(){
-        this.tab = 1;
+    app.directive('greetingTab', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'greeting-tab/greeting-tab.html',
+            controller: function() {
+                this.tab = 1;
 
-        this.setTab = function(newValue){
-            this.tab = newValue;
-        };
+                this.setTab = function (newValue) {
+                    this.tab = newValue;
+                };
 
-        this.isSet = function(tabName){
-            return this.tab === tabName;
+                this.isSet = function (tabName) {
+                    return this.tab === tabName;
+                }
+            },
+            controllerAs: 'tab'
         };
     });
 
