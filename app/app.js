9394808cc4484dfe32118e0e787d66c5f358438f
rgarcia <rgarcia@cambridgesemantics.com>
2014-11-30 21:31:15 -0500

    GreetingsJS now works on top of the JSON API of the GreetingsApp Spring application. Currently supporting listing greetings and creating new ones.

--- app/app.js
+++ app/app.js
@@ -3,29 +3,29 @@
  */
 
 (function(){
-    var greetings = [
-        {"id":1, "content":"Hello World!", "user":"determined@greetings.com", "when":Date.UTC(1970,1,1)},
-        {"id":2, "content":"This is a way to extend the farewell before saying... goodbye!",
-            "user":"hesitant@greetings.com", "when":Date.UTC(2000,1,1)} ];
-
     var app = angular.module("greetingsJS", ["greetingTab","greetingForm"]);
 
-    app.controller("GreetingsController", function(){
-        this.greetings = greetings;
+    app.controller("GreetingsController", ["$http",
+        function($http) {
+            this.GREETINGS_API = "http://greetings-app.herokuapp.com/greetings";
+            this.newGreeting = {'date': Date.now()};
+            var greetingCtrl = this;
 
+            this.listGreetings = function()
+            {
+                $http.get("http://greetings-app.herokuapp.com/greetings.json")
+                    .success(function (data) {
+                        greetingCtrl.greetings = data;
+                    });
+            }
+            this.listGreetings();
 
-        this.clearGreeting = function(){
-            return {"content":"Greeting text...", "user":"Your E-Mail", "when":Date.now()};
-        }
-
-        this.newGreeting = this.clearGreeting();
-
-        this.addGreeting = function(){
-            this.newGreeting.id = greetings.length + 1;
-            this.newGreeting.when = Date.now();
-            this.greetings.push(this.newGreeting);
-            this.newGreeting = this.clearGreeting();
-        };
-    });
-
-})();
+            this.addGreeting = function(){
+                $http.post("http://greetings-app.herokuapp.com/greetings", this.newGreeting)
+                    .then(function(){
+                        greetingCtrl.newGreeting = {'date': Date.now()};
+                        greetingCtrl.listGreetings();
+                    });
+            };
+        }]);
+}());
