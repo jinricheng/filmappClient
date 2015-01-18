c4fc7eff9e1a92701c1853334bdb86e0e08ba33f
Mauro Churata <xurata@gmail.com>
2015-01-18 17:22:48 +0100

    updating app.js

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@
 
     app.controller("GreetingsController", ["$http",
         function($http) {
-            this.GREETINGS_API = "https://filmsapp.herokuapp.com/films";
+            this.FILMS_API = "https://filmsapp.herokuapp.com/films";
             this.newGreeting = {'date': Date.now()};
             this.loading = false;
             var  filmCtrl = this;
@@ -22,17 +22,17 @@
 
             this.listGreetings = function(){
                 this.loading = true;
-                $http.get(this.GREETINGS_API)
+                $http.get(this.FILMS_API)
                     .success(function (data) {
                         filmCtrl.films = data;
                     });
             };
 
             this.addGreeting = function(){
-                $http.post(this.GREETINGS_API, this.newGreeting)
+                $http.post(this.FILMS_API, this.newGreeting)
                     .then(function(){
-                        greetingCtrl.newGreeting = {'date': Date.now()};
-                        greetingCtrl.listGreetings();
+                        filmCtrl.newGreeting = {'date': Date.now()};
+                        filmCtrl.listGreetings();
                     });
             };
         }]);
